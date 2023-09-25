0d038701e04cb87e2d5dbc9de3a466ecc1411fa4
baqiyansheng <2536695983@qq.com>
2023-09-25 23:20:12 +0800

    导入表格id并实现电影信息搜集

--- craft.py
+++ craft.py
@@ -1,6 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import random
+import pandas as pd
+import time
+
+# 读取Excel文件
+df = pd.read_csv('web_lab-1/Book_id.csv')
+# 选择要读取的列
+book_id_data = df['1046265'].tolist()
+book_id_data.insert(0, '1046265')
+
+df = pd.read_csv('web_lab-1/Movie_id.csv')
+movie_id_data = df['1292052'].tolist()
+movie_id_data.insert(0, '1292052')
+delay = random.randint(0, 1200)
 
 
 def random_ip():
@@ -17,46 +30,13 @@ headers = {
 }
 
 
-# 给定名字搜索，对搜索结果进行分析
-def search_douban_movie(movie_name):
+# 给定id搜索结果
+def search_douban_movie(id):
     # 构造搜索URL
-    search_url = f"https://www.douban.com/search?q={movie_name}"
+    search_url = f"https://movie.douban.com/subject/{id}/"
     # ip = random_ip()
     # 发送HTTP请求并获取页面内容
     response = requests.get(search_url, headers=headers)
-    if response.status_code != 200:
-        print("请求失败")
-        return
-
-    # 使用BeautifulSoup解析页面内容
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # 提取搜索结果中的电影信息
-    results = soup.find_all("div", class_="result")
-    for result in results:
-        # 随机生成IP
-        ip = random_ip()
-        headers["X-Forwarded-For"] = ip  # 将随机生成的IP添加到报头中
-        # 获取条目类型
-        type = result.find("h3").text.strip()
-        type = type.split()[0]
-        if (type[0] != '['):
-            return
-        begin = int(type.index('['))
-        end = int(type.index(']'))
-        type = type[begin + 1:end]
-        if (type != '电影' and type != '电视剧' and type != '书籍'):
-            return
-        # 获取条目
-        content = result.find("div", class_="content")
-        # 获取条目的链接
-        next_url = content.find("a").get("href")
-        more_info(next_url)
-        print("=" * 40)
-
-
-def more_info(next_url):
-    response = requests.get(next_url, headers=headers)
     if response.status_code != 200:
         print("请求失败")
     # 使用BeautifulSoup解析页面内容
@@ -84,19 +64,28 @@ def more_info(next_url):
     index_4 = info.find('类型: ')
     index_5 = info.find('制片国家/地区: ')
     index_6 = info.find('语言: ')
-    index_7 = info.find('首播: ')
+    index_7 = info.find('\n', index_6)
     print('导演:', info[index_1 + len('导演: '):index_2 - 1])
     print('编剧:', info[index_2 + len('编剧: '):index_3 - 1])
     print('主演:', info[index_3 + len('主演: '):index_4 - 1])
     print('类型:', info[index_4 + len('类型: '):index_5 - 1])
     print('制片国家/地区:', info[index_5 + len('制片国家/地区: '):index_6 - 1])
-    print('语言:', info[index_6 + len('语言: '):index_7 - 1])
-    detail = soup.find('span', {'property': 'v:summary'}).text.replace(' ', '')
-    print('简介:', detail)
-    pass
+    print('语言:', info[index_6 + len('语言: '):index_7])
+    detail = soup.find('span', {'class': 'all hidden'})  # 展开简介
+    if (detail is None):  # 不需要展开简介
+        detail = soup.find('span', {'property': 'v:summary'})
+    detail = detail.text.strip().replace(' ', '')
+    detail = detail.replace('\n\u3000\u3000', '')
+    print('简介:\n', detail)
+    print("=" * 40)
 
 
 if __name__ == "__main__":
-    # movie_name = "憨豆先生"
-    movie_name = input("请输入要搜索的电影名称: ")
-    search_douban_movie(movie_name)
+    for id in movie_id_data:
+        search_douban_movie(id)
+        delay = random.randint(0, 5)  # 随机间隔0-5s访问
+        time.sleep(delay)
+    for id in book_id_data:
+        search_douban_movie(id)
+        delay = random.randint(0, 5)  # 随机间隔0-5s访问
+        time.sleep(delay)
