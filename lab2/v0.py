e7b71876a09bd87c51d017f074e981c9d84b0802
baqiyansheng <2536695983@qq.com>
2023-11-22 15:52:42 +0800

    lab2-part1

--- lab2/v0.py
+++ lab2/v0.py
@@ -1,136 +1,140 @@
-import copy
 import gzip
 import json
 import os
-
 import pandas as pd
 import csv
+import time
 
-# 构建movie,tag字典
+# 开始计时
+start_time = time.time()
+
+# 设定参数:允许遍历的最大次数
+max_run_times = 1000000
+
+# 读取Movie_tag.csv文件，构建movie字典
 movie_tags = pd.read_csv("./lab2/Movie_tag.csv")
-movie = {}
-for i in range(len(movie_tags)):
-    movie[movie_tags["id"][i]] = movie_tags['tag'][i]
+movie = {
+    movie_tags["id"][i]: movie_tags['tag'][i]
+    for i in range(len(movie_tags))
+}
+template_str = "http://rdf.freebase.com/ns/"
 
-# print(movie.keys())
+# 构建实体_tag字典
+movie_entity = {}
+mvi_entities = set()  # 初始可匹配实体
+mvi_entities2 = set()  # 一跳后可匹配实体
+mvi_entities3 = set()  # 二跳后可匹配实体
+
 # 记录实体相关三元组数量
 entry_num = {}
 
-movie_entity = {}
-mvi_entities = []  # 初始可匹配实体
-mvi_entities2 = []  # 一跳后可匹配实体
-mvi_entities3 = []  # 二跳后可匹配实体
+# 构建实体-tag字典，同时匹配获得Freebase中对应的实体（共578个可匹配实体），加入到一跳可匹配实体
 with open('./lab2/douban2fb.txt', 'rb') as f:
     for line in f:
         line = line.strip()
-        list1 = line.decode().split('\t')  # 电影ID与实体的字典
-        mvi_entities.append(list1[1])
-        mvi_entities2.append(list1[1])
-        # mvi_entities3.append(list1[1])
-        movie_entity[list1[1]] = {"tag": movie[int(list1[0])]}  # 构建实体_tag字典
+        parts = line.decode().split('\t')
+        mvi_entities.add(parts[1])
+        # mvi_entities2.add(parts[1])
+        # mvi_entities3.add(parts[1])
+        movie_entity[parts[1]] = {"tag": movie[int(parts[0])]}  # 构建实体_tag字典
+
 # 将实体_tag字典转化为json格式
 data = json.dumps(movie_entity, indent=1, ensure_ascii=False)
-# print(data)
 with open('movie_entity.json', 'w', newline='\n') as f:
     f.write(data)
-f.close()
 
+# 提取一跳可达实体
 freebase_info_fpath = "./lab2/freebase_douban.gz"  # 初始freebase
 outfile1 = "./lab2/graph_1step.txt"  # 一跳输出文件
 outfile2 = "./lab2/graph_2step.txt"  # 二跳输出文件
-
-template_str = "http://rdf.freebase.com/ns/"
-
-if os.path.exists(outfile1):
-    os.remove(outfile1)
-wf = open(outfile1, "ab")
+outfile3 = "./lab2/fb.txt"
+# if os.path.exists(outfile1):
+#     os.remove(outfile1)
+wf = open(outfile1, "w")
 count = 0
 with gzip.open(freebase_info_fpath, 'rb') as f:
+    count = 0
     for line in f:
-        count = count + 1
-        # if count > 100000:
-        #     break
-        line_info = line.decode().split('\t')
+        count += 1
+        if count > max_run_times:
+            break  # 超过设定上限即停止查找一跳可达实体
         # 过滤不含有template前缀的实体
-        if (template_str not in line_info[0]) or (template_str
-                                                  not in line_info[2]):
+        triple_parts = line.decode().split('\t')
+        if (template_str not in triple_parts[0]) or (template_str
+                                                     not in triple_parts[2]):
             continue
-        # print(line)
-        # 提取"http://rdf.freebase.com/ns/"之后的内容:
         # 头实体
-        head = line_info[0][len(template_str) + 1:].strip('>')
+        head = triple_parts[0][len(template_str) + 1:].strip('>')
         # 尾实体
-        tail = line_info[2][len(template_str) + 1:].strip('>')
+        tail = triple_parts[2][len(template_str) + 1:].strip('>')
+
         if (head in mvi_entities) or (tail in mvi_entities):
-            wf.write(line)
-            if (head not in mvi_entities) and (head not in mvi_entities2):
-                mvi_entities2.append(head)
-            if (tail not in mvi_entities) and (tail not in mvi_entities2):
-                mvi_entities2.append(tail)
-            if (head in entry_num):
-                entry_num[head] = entry_num[head] + 1
-                # print(head, entry_num[head])
-            else:
-                entry_num[head] = 1
-            if (tail in entry_num):
-                entry_num[tail] = entry_num[tail] + 1
-                # print(tail, entry_num[tail])
-            else:
-                entry_num[tail] = 1
-wf.close()
-
-# print(mvi_entities2)
+            with open(outfile1, "ab") as wf1:
+                wf1.write(line)  # 写入一跳可达实体
+                if (head not in mvi_entities) and (head not in mvi_entities2):
+                    mvi_entities2.add(head)
+                if (tail not in mvi_entities) and (tail not in mvi_entities2):
+                    mvi_entities2.add(tail)
+                # 实体相关三元组计数
+                if (head in entry_num):
+                    entry_num[head] += 1
+                else:
+                    entry_num[head] = 1
+                if (tail in entry_num):
+                    entry_num[tail] += 1
+                else:
+                    entry_num[tail] = 1
 print("done1")
+# # 将一跳可达实体写入二跳可达的实体
+# with open(outfile3, "w") as wf:
+#     for line in mvi_entities2:
+#         wf.write(line + '\n')
 
-template_str = "http://rdf.freebase.com/ns/"
-if os.path.exists(outfile2):
-    os.remove(outfile2)
-wf = open(outfile2, "ab")
-count = 0
+# if os.path.exists(outfile2):
+#     os.remove(outfile2)
 with gzip.open(freebase_info_fpath, 'rb') as f:
+    count = 0
     for line in f:
-        count = count + 1
-        # if count > 100000:
-        #     break
-        line_info = line.decode().split('\t')
+        count += 1
+        if count > max_run_times:
+            break
         # 过滤不含有template前缀的实体
-        if (template_str not in line_info[0]) or (template_str
-                                                  not in line_info[2]):
+        triple_parts = line.decode().split('\t')
+        if (template_str not in triple_parts[0]) or (template_str
+                                                     not in triple_parts[2]):
             continue
-        # print(line)
-        # 提取"http://rdf.freebase.com/ns/"之后的内容:
-        # 头实体
-        head = line_info[0][len(template_str) + 1:].strip('>')
-        # 尾实体
-        tail = line_info[2][len(template_str) + 1:].strip('>')
+        head = triple_parts[0][len(template_str) + 1:].strip('>')
+        tail = triple_parts[2][len(template_str) + 1:].strip('>')
+
         if (head in mvi_entities2) or (tail in mvi_entities2):
-            wf.write(line)
-            if (head not in mvi_entities2) and (head not in mvi_entities3):
-                mvi_entities3.append(head)
-            if (tail not in mvi_entities2) and (tail not in mvi_entities3):
-                mvi_entities3.append(tail)
-            if (head in entry_num):
-                entry_num[head] = entry_num[head] + 1
-            else:
-                entry_num[head] = 1
-            if (tail in entry_num):
-                entry_num[tail] = entry_num[tail] + 1
-            else:
-                entry_num[tail] = 1
-wf.close()
-# print(mvi_entities3)
+            with open(outfile2, "ab") as wf2:
+                wf2.write(line)
+
+                if (head not in mvi_entities2) and (head not in mvi_entities3):
+                    mvi_entities3.add(head)
+                if (tail not in mvi_entities2) and (tail not in mvi_entities3):
+                    mvi_entities3.add(tail)
+
+                if (head in entry_num):
+                    entry_num[head] += 1
+                else:
+                    entry_num[head] = 1
+                if (tail in entry_num):
+                    entry_num[tail] += 1
+                else:
+                    entry_num[tail] = 1
 print("done2")
 
-outfile3 = "./lab2/fb.txt"
-if os.path.exists(outfile3):
-    os.remove(outfile3)
-wf = open(outfile3, "w")
-
-# 计算实体相关三元组数，初始的578个实体不能直接加入到2跳集合中，因为有可能它没有相关的三元组
-for line in mvi_entities3:
-    if (entry_num[line] < 10):  # 只保留出现频率高于50的实体
-        continue
-    wf.write(line + '\n')
+# 写入删选后的实体
+# if os.path.exists(outfile3):
+#     os.remove(outfile3)
+with open(outfile3, "w") as wf:
+    for line in mvi_entities3:
+        if entry_num[line] >= 10:  # 频率高于10才能被选中
+            wf.write(line + '\n')
+            # print(line, entry_num[line])
 print('done3')
-# print(entry_num)
-wf.close()
+
+# 结束时间
+end_time = time.time()
+print("time:", end_time - start_time)
